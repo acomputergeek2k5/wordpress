6941557efd8a96a54fd0349a363d074822347fdf
Zac Harris <acomputergeek2k5@gmail.com>
2020-05-15 12:55:52 -0400

    Added ability to Create Professors
    Added the ability to create Professors for the academy.
    Added the ability to customize page banner subtitle.
    Added the ability to customize page banner image.

--- AcademyTheme/functions.php
+++ AcademyTheme/functions.php
@@ -10,7 +10,11 @@ function academy_files() {
 add_action('wp_enqueue_scripts', 'academy_files');
 
 function academy_features() {
-	add_theme_support('title-tag');
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	add_image_size( 'professorLandscape', 400, 260, true );
+	add_image_size( 'professorPortrait', 480, 650, true );
+	add_image_size( 'pageBanner', 1500, 350, true );
 	register_nav_menu('headerMenuLocation', 'Header Menu Location');
 	register_nav_menu('footerLocationOne', 'Footer Location One');
 	register_nav_menu('footerLocationTwo', 'Footer Location Two');
