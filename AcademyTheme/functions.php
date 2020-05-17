c17dea56f3dd8bc92e2ce90c138dba4429b64e50
Zac Harris <acomputergeek2k5@gmail.com>
2020-05-17 14:05:27 -0400

    Added users & permissions
    Added the ability to control users and permissions with specialty roles.
    
    Added the ability to allow secure public registration.
    
    Customized the login screen to take on the styling of the main site.

--- AcademyTheme/functions.php
+++ AcademyTheme/functions.php
@@ -104,3 +104,46 @@ function academyMapKey($api) {
 }
 
 add_filter( 'acf/fields/google_map/api', 'academyMapKey' );
+
+
+// Redirect subscriber accounts out of admin and onto homepage
+add_action( 'admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend() {
+	$ourCurrentUser = wp_get_current_user();
+	
+	if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+		wp_redirect( site_url( '/' ) );
+		exit;
+	}
+}
+
+add_action( 'wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar() {
+	$ourCurrentUser = wp_get_current_user();
+	
+	if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+		show_admin_bar( false );
+	}
+}
+
+// Customize Login Screen
+add_filter( 'login_headerurl', 'ourHeaderUrl' );
+
+function ourHeaderUrl() {
+	return esc_url( site_url('/'));
+}
+
+add_action( 'login_enqueue_scripts', 'ourLoginCSS' );
+
+function ourLoginCSS() {
+	wp_enqueue_style( 'academy_main_styles', get_stylesheet_uri() );
+	wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+}
+
+add_filter( 'login_headertitle', 'academyLoginTitle');
+
+function academyLoginTitle() {
+	return get_bloginfo( 'name');
+}
